ac152a74c87eeab455bf53dcf38c7bc6b4f30ab9
Edward Wang <wlz1028@gmail.com>
2015-01-25 22:53:30 -0500

    up

--- .bash_profile
+++
@@ -1,6 +0,0 @@
-set autolist
-alias v="gvim &> /dev/null"
-alias ll='ls -l'
-alias ls='ls -G'
-alias startSimpleServer='python -m SimpleHTTPServer'
-#test
--- init.sh
+++ init.sh
@@ -3,13 +3,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
-sudo apt-get update
-sudo apt-get install vim
-sudo apt-get install vim-gnome
+#sudo apt-get update
+#sudo apt-get install vim
+#sudo apt-get install vim-gnome
 
 #echo Copy profiles to home dir
 for i in `find . -type f -name ".*"`;
 do
-  echo $i
-  cp -a $i ~
+  ln -s  $DIR/$i $HOME/$i
 done
